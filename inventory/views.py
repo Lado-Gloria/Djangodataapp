544e3bee7de347601307104c01feb5c122d54042
Lado-Gloria <glorialadoo@gmail.com>
2023-07-21 14:08:16 +0300

    changes

--- inventory/views.py
+++ inventory/views.py
@@ -4,5 +4,15 @@ from .forms import ProductUploadForm
 # Create your views here.
 
 def upload_product(request):
-    form =ProductUploadForm()
+    form =ProductUploadForm() 
+    if request.method =="POST":
+        form  = ProductUploadForm(request.POST)
+        if form .is_valid():
+            form.save()
+
+
+        else:
+            form =ProductUploadForm()
     return render(request, "inventory/product_upload.html",{"form":form})
+
+
